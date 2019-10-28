25e2f3f78ca95698e50cb1f41eade86672bd80ed
Ammarah <42020953+ammarahnizam@users.noreply.github.com>
2019-10-29 00:13:31 +0500

    Create report.js

---
+++ JunitCucumberTsk/report.js
@@ -0,0 +1,294 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("AllFeatures.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Rental Application",
+  "description": "",
+  "id": "rental-application",
+  "keyword": "Feature"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "Open Browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "Enter the url \"https://acme-qa.everreal.co/app/public/apply/d986458c-4423-4441-be64-77774109864b/applications/step2\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.open_Browser()"
+});
+formatter.result({
+  "duration": 1605949788,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "https://acme-qa.everreal.co/app/public/apply/d986458c-4423-4441-be64-77774109864b/applications/step2",
+      "offset": 15
+    }
+  ],
+  "location": "StepDefinition.enter_the_url(String)"
+});
+formatter.result({
+  "duration": 4972403526,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 8,
+  "name": "To verify the Page Title of Rental Application is \"Demo\"",
+  "description": "",
+  "id": "rental-application;to-verify-the-page-title-of-rental-application-is-\"demo\"",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 9,
+  "name": "View Page Title",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.view_Page_Title()"
+});
+formatter.result({
+  "duration": 96008346,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "Open Browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "Enter the url \"https://acme-qa.everreal.co/app/public/apply/d986458c-4423-4441-be64-77774109864b/applications/step2\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.open_Browser()"
+});
+formatter.result({
+  "duration": 1006827733,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "https://acme-qa.everreal.co/app/public/apply/d986458c-4423-4441-be64-77774109864b/applications/step2",
+      "offset": 15
+    }
+  ],
+  "location": "StepDefinition.enter_the_url(String)"
+});
+formatter.result({
+  "duration": 4641703194,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 12,
+  "name": "To verify the URL of Rental Application is correct",
+  "description": "",
+  "id": "rental-application;to-verify-the-url-of-rental-application-is-correct",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 13,
+  "name": "View URL",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.view_URL()"
+});
+formatter.result({
+  "duration": 88810461,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "Open Browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "Enter the url \"https://acme-qa.everreal.co/app/public/apply/d986458c-4423-4441-be64-77774109864b/applications/step2\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.open_Browser()"
+});
+formatter.result({
+  "duration": 976002647,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "https://acme-qa.everreal.co/app/public/apply/d986458c-4423-4441-be64-77774109864b/applications/step2",
+      "offset": 15
+    }
+  ],
+  "location": "StepDefinition.enter_the_url(String)"
+});
+formatter.result({
+  "duration": 5692807416,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 17,
+  "name": "To verify the \"House Hold Information\" tab is highlighted",
+  "description": "",
+  "id": "rental-application;to-verify-the-\"house-hold-information\"-tab-is-highlighted",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 18,
+  "name": "View tab",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.view_tab()"
+});
+formatter.result({
+  "duration": 118234251,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "Open Browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "Enter the url \"https://acme-qa.everreal.co/app/public/apply/d986458c-4423-4441-be64-77774109864b/applications/step2\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.open_Browser()"
+});
+formatter.result({
+  "duration": 976204408,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "https://acme-qa.everreal.co/app/public/apply/d986458c-4423-4441-be64-77774109864b/applications/step2",
+      "offset": 15
+    }
+  ],
+  "location": "StepDefinition.enter_the_url(String)"
+});
+formatter.result({
+  "duration": 5752951463,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 20,
+  "name": "To verify error validation message must appear if user tries to proceed without selecting any value in all three drop-downs",
+  "description": "",
+  "id": "rental-application;to-verify-error-validation-message-must-appear-if-user-tries-to-proceed-without-selecting-any-value-in-all-three-drop-downs",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 21,
+  "name": "View Error Validation Messages",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.view_Error_Validation_Messages()"
+});
+formatter.result({
+  "duration": 301856656,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "Open Browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "Enter the url \"https://acme-qa.everreal.co/app/public/apply/d986458c-4423-4441-be64-77774109864b/applications/step2\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.open_Browser()"
+});
+formatter.result({
+  "duration": 985621100,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "https://acme-qa.everreal.co/app/public/apply/d986458c-4423-4441-be64-77774109864b/applications/step2",
+      "offset": 15
+    }
+  ],
+  "location": "StepDefinition.enter_the_url(String)"
+});
+formatter.result({
+  "duration": 12936742618,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 24,
+  "name": "To verify the EverReal logo is redirecting towards EverReal Home Page",
+  "description": "",
+  "id": "rental-application;to-verify-the-everreal-logo-is-redirecting-towards-everreal-home-page",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 25,
+  "name": "Check Logo",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.check_Logo()"
+});
+formatter.result({
+  "duration": 3382646235,
+  "status": "passed"
+});
+});
